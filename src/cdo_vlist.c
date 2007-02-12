256a822db99e8a22d61b0758a921ab6127c06a07
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2007-02-12 10:37:58 +0000

    vlistCompare: check grid orientation for func_sft

--- src/cdo_vlist.c
+++ src/cdo_vlist.c
@@ -17,11 +17,13 @@
 
 #include "cdi.h"
 #include "cdo.h"
+#include "cdo_int.h"
 #include "error.h"
 #include "functs.h"
 
 void vlistCompare(int vlistID1, int vlistID2, int function)
 {
+  static char func[] = "vlistCompare";
   int varID, nvars;
 
   if ( vlistNvars(vlistID1) != vlistNvars(vlistID2) )
@@ -31,6 +33,7 @@ void vlistCompare(int vlistID1, int vlistID2, int function)
     cdoAbort("Input streams have different number of records per timestep!");
 
   nvars = vlistNvars(vlistID1);
+
   if ( function == func_hrd )
     for ( varID = 0; varID < nvars; varID++ )
       {
@@ -52,16 +55,57 @@ void vlistCompare(int vlistID1, int vlistID2, int function)
 	  cdoAbort("Number of levels of the input fields do not match!");
       }
   else if ( function == func_sft )
-    for ( varID = 0; varID < nvars; varID++ )
-      {
-	if ( gridInqSize(vlistInqVarGrid(vlistID1, varID)) !=
-	     gridInqSize(vlistInqVarGrid(vlistID2, varID)) )
-	  cdoAbort("Grid size of the input fields do not match!");
+    {
+      for ( varID = 0; varID < nvars; varID++ )
+	{
+	  if ( gridInqSize(vlistInqVarGrid(vlistID1, varID)) !=
+	       gridInqSize(vlistInqVarGrid(vlistID2, varID)) )
+	    cdoAbort("Grid size of the input fields do not match!");
 
-	if ( zaxisInqSize(vlistInqVarZaxis(vlistID1, varID)) !=
-	     zaxisInqSize(vlistInqVarZaxis(vlistID2, varID)) )
-	  cdoAbort("Number of levels of the input fields do not match!");
+	  if ( zaxisInqSize(vlistInqVarZaxis(vlistID1, varID)) !=
+	       zaxisInqSize(vlistInqVarZaxis(vlistID2, varID)) )
+	    cdoAbort("Number of levels of the input fields do not match!");
+	}
+      
+      /* compare grids of first variable */
+      {
+	int gridID1, gridID2;
+	int xsize, ysize;
+	gridID1 = vlistInqVarGrid(vlistID1, 0);
+	gridID2 = vlistInqVarGrid(vlistID2, 0);
+
+	if ( gridInqType(gridID1) == GRID_GAUSSIAN || gridInqType(gridID1) == GRID_LONLAT )
+	  {
+	    xsize = gridInqXsize(gridID1);
+	    ysize = gridInqYsize(gridID1);
+
+	    if ( ysize == gridInqYsize(gridID2) )
+	      {
+		double *yvals1, *yvals2;
+
+		yvals1 = (double *) malloc(ysize*sizeof(double));
+		yvals2 = (double *) malloc(ysize*sizeof(double));
+
+		gridInqYvals(gridID1, yvals1);
+		gridInqYvals(gridID2, yvals2);
+		
+		if ( DBL_IS_EQUAL(yvals1[0], yvals2[ysize-1]) &&
+		     DBL_IS_EQUAL(yvals1[ysize-1], yvals2[0]) )
+		  {
+		    if ( yvals1[0] > yvals2[0] )
+		      cdoWarning("Grid orientation differ! First grid: N->S; second grid: S->N");
+		    else
+		      cdoWarning("Grid orientation differ! First grid: S->N; second grid: N->S");
+		  }
+
+		free(yvals1);
+		free(yvals2);
+	      }
+	    else
+	      cdoWarning("ysize of input grids differ!");
+	  }
       }
+    }
   else
     cdoAbort("Internal problem! Invalid function %d", function);
 }
