166213250c5476b2e8c4bab3fcf6246af0b29ac7
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2008-03-13 10:55:48 +0000

    splitsel: bug fix for multilevel/multivar datasets

--- src/Splitsel.c
+++ src/Splitsel.c
@@ -2,7 +2,7 @@
   This file is part of CDO. CDO is a collection of Operators to
   manipulate and analyse Climate model Data.
 
-  Copyright (C) 2003-2006 Uwe Schulzweida, Uwe.Schulzweida@zmaw.de
+  Copyright (C) 2003-2008 Uwe Schulzweida, Uwe.Schulzweida@zmaw.de
   See COPYING file for copying and redistribution conditions.
 
   This program is free software; you can redistribute it and/or modify
@@ -72,7 +72,7 @@ void *Splitsel(void *argument)
 
   nargc = operatorArgc();
 
-  if (nargc<1)
+  if ( nargc < 1 )
     cdoAbort("Too few arguments! Need %d found %d.", 1, nargc);
 
 /*   ndates = atoi(operatorArgv()[0]); */
@@ -125,17 +125,6 @@ void *Splitsel(void *argument)
   nsets = 0;
   while ( TRUE )
     {
-/*       for ( nsets = 0; nsets < ndates; nsets++ ) */
-/* 	{ */
-/*       printf("index: %d tsID: %d nrecs: %d\n",index,tsID,nrecs); */
-/*       nrecs = streamInqTimestep(streamID1, tsID); */
-      //      if ( nrecs == 0 ) break;
-
-      vdate = taxisInqVdate(taxisID1);
-      vtime = taxisInqVtime(taxisID1);
-
-/*       printf("vdate: %d vtime: %d\n",vdate,vtime); */
-
       sprintf(filename+nchars, "%03d", index);
       sprintf(filename+nchars+3, "%s", filesuffix);
 	  
@@ -144,23 +133,27 @@ void *Splitsel(void *argument)
       if ( streamID2 < 0 ) cdiError(streamID2, "Open failed on %s", filename);
 
       streamDefVlist(streamID2, vlistID2);
-      tsID2 = 0;
 
+      tsID2 = 0;
 
 /*       printf("comp: %f %d\n",ndates*(index+1),(int)(ndates*(index+1))); */
       for ( ; nsets < (int)(ndates*(index+1)); nsets++ ) 
 	{
 
 	  nrecs = streamInqTimestep(streamID1, tsID);
-/* 	  printf( "nsets: %d mod: %d\n",nsets, nsets % (int)(ndates) ); */
+	  if ( nrecs == 0 ) break;
+
+	  vdate = taxisInqVdate(taxisID1);
+	  vtime = taxisInqVtime(taxisID1);
+
+	  /* printf("vdate: %d vtime: %d\n", vdate, vtime); */
+
+	  taxisCopyTimestep(taxisID2, taxisID1);
+	  streamDefTimestep(streamID2, tsID2);
 
 	  for ( recID = 0; recID < nrecs; recID++ )
 	    {
 	      
-	      taxisCopyTimestep(taxisID2, taxisID1);
-	      
-/* 	      streamDefTimestep(streamID2, nsets % (int)ndates); */
-	      streamDefTimestep(streamID2, tsID2);
 	      streamInqRecord(streamID1, &varID, &levelID);
 	      streamDefRecord(streamID2,  varID,  levelID);
 	      if ( lcopy )
@@ -179,6 +172,7 @@ void *Splitsel(void *argument)
 	}
       
       streamClose(streamID2);
+      if ( nrecs == 0 ) break;
 
       nrecs = streamInqTimestep(streamID1, tsID);
       if ( nrecs == 0 ) break;
