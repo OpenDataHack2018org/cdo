488f403c32fc8828d1951beeff2f070301ea9c41
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2010-03-02 13:37:27 +0000

    module Ymonarith: remove limitation to non spectral fields

--- src/Ymonarith.c
+++ src/Ymonarith.c
@@ -73,9 +73,6 @@ void *Ymonarith(void *argument)
   vlistID3 = vlistDuplicate(vlistID1);
 
   vlistCompare(vlistID1, vlistID2, func_sft);
-  
-  if ( operfunc == func_mul || operfunc == func_div )
-    nospec(vlistID1);
 
   gridsize = vlistGridsizeMax(vlistID1);
 
