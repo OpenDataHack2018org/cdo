cc7ed546e7d2210bd1c6cc94df40ac5c6f9ffbc1
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2010-03-02 13:35:21 +0000

    module Math: remove limitation to non spectral fields

--- src/Math.c
+++ src/Math.c
@@ -2,7 +2,7 @@
   This file is part of CDO. CDO is a collection of Operators to
   manipulate and analyse Climate model Data.
 
-  Copyright (C) 2003-2009 Uwe Schulzweida, Uwe.Schulzweida@zmaw.de
+  Copyright (C) 2003-2010 Uwe Schulzweida, Uwe.Schulzweida@zmaw.de
   See COPYING file for copying and redistribution conditions.
 
   This program is free software; you can redistribute it and/or modify
@@ -97,8 +97,6 @@ void *Math(void *argument)
   taxisID1 = vlistInqTaxis(vlistID1);
   taxisID2 = taxisDuplicate(taxisID1);
   vlistDefTaxis(vlistID2, taxisID2);
-
-  nospec(vlistID1);
 
   gridsize = vlistGridsizeMax(vlistID1);
 
