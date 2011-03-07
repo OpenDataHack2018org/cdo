c5b1a2bf6caa28fa83479ea2ee0228f5b0a53f12
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2011-03-07 08:33:32 +0000

    gridToCell(GME): set X/Y units to degrees_east/degrees_north

--- src/grid.c
+++ src/grid.c
@@ -2,7 +2,7 @@
   This file is part of CDO. CDO is a collection of Operators to
   manipulate and analyse Climate model Data.
 
-  Copyright (C) 2003-2010 Uwe Schulzweida, Uwe.Schulzweida@zmaw.de
+  Copyright (C) 2003-2011 Uwe Schulzweida, Uwe.Schulzweida@zmaw.de
   See COPYING file for copying and redistribution conditions.
 
   This program is free software; you can redistribute it and/or modify
@@ -983,8 +983,8 @@ int gridToUnstructured(int gridID1)
 	gridDefXbounds(gridID2, xbounds);
 	gridDefYbounds(gridID2, ybounds);
 
-	gridDefXunits(gridID2, "degrees");
-	gridDefYunits(gridID2, "degrees");
+	gridDefXunits(gridID2, "degrees_east");
+	gridDefYunits(gridID2, "degrees_north");
 
 	free (imask);
 	free (xvals);
