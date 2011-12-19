6eb79e9ee183420a1dc736855ea613f610603c39
Ralf Mueller <ralf.mueller@mpimet.mpg.de>
2011-12-19 16:25:43 +0000

    Test for operator call

--- contrib/python/cdo.py
+++ contrib/python/cdo.py
@@ -2,13 +2,25 @@ from os import system,popen
 import re
 import subprocess
 
-method_dictionary = ["sinfov"]
+method_dictionary = ["sinfov","remap"]
 
 class Cdo(object):
+
+    def getOperators(self):
+        proc = subprocess.Popen(['cdo','-h'],stderr = subprocess.PIPE)
+        ret  = proc.communicate()
+        l    = ret[1].find("Operators:")
+        ops  = ret[1][l:-1].split("\n")[1:-1]
+        endI = ops.index('')
+        s    = ' '.join(ops[:endI]).strip()
+        s    = re.sub("\s+" , " ", s)
+        return s.split(" ")
+
     def __init__(self):
         """
             Store params and junk. Ordinarily more verbose.
         """
+        self.operators = self.getOperators()
     
     def __getattr__(self, method_name):
         """
@@ -20,8 +32,8 @@ class Cdo(object):
         """
         def get(self, *args):
             # Make our API calls, return data, etc
-            for arg in args:
-              print arg
+            print method_name
+            print ' '.join(['cdo', ','.join([method_name, ','.join(args)])])
 
             return
         
@@ -31,24 +43,14 @@ class Cdo(object):
             # If the method isn't in our dictionary, act normal.
             raise AttributeError, method_name
 
-    def getOperators(self):
-      proc = subprocess.Popen(['cdo','-h'],stderr = subprocess.PIPE)
-      ret  = proc.communicate()
-      l    = ret[1].find("Operators:")
-      ops  = ret[1][l:-1].split("\n")[1:-1]
-      endI = ops.index('')
-      s    = ' '.join(ops[:endI]).strip()
-      s    = re.sub("\s+" , " ", s)
-      return s.split(" ")
-
 
 if __name__ == '__main__':
     cdo = Cdo()
-    s = cdo.getOperators()
+    s = cdo.operators
     print "#=============================================================="
     for ss in s:
-      print "#=============================================================="
       print ss
 
     # Call an arbitrary method.
-    cdo.sinfov(1,'str')
+    cdo.sinfov('1','str')
+    cdo.remap('gridFile','weightfile')
