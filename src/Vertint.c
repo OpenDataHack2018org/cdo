acd9297f8e8d2a187b6e3d8283c9a6ff231ef66f
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2012-02-15 13:51:07 +0000

    ml2pl: check missing values

--- src/Vertint.c
+++ src/Vertint.c
@@ -2,7 +2,7 @@
   This file is part of CDO. CDO is a collection of Operators to
   manipulate and analyse Climate model Data.
 
-  Copyright (C) 2003-2011 Uwe Schulzweida, Uwe.Schulzweida@zmaw.de
+  Copyright (C) 2003-2012 Uwe Schulzweida, Uwe.Schulzweida@zmaw.de
   See COPYING file for copying and redistribution conditions.
 
   This program is free software; you can redistribute it and/or modify
@@ -598,6 +598,12 @@ void *Vertint(void *argument)
 		      param = vlistInqVarParam(vlistID1, varID);
 		      cdiParamToString(param, paramstr, sizeof(paramstr));
 		      cdoAbort("Number of hybrid level differ from full/half level (param=%s)!", paramstr);
+		    }
+
+		  for ( levelID = 0; levelID < nlevel; levelID++ )
+		    {
+		      if ( varnmiss[varID][levelID] )
+			cdoAbort("Missing values unsupported for this operator!");
 		    }
 
 		  if ( varID == tempID )
