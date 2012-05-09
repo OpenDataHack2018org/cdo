a66986fa4255a749f67934f3a7017b392abc5993
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2012-05-09 09:51:02 +0000

    Vargen: init varID2

--- src/Vargen.c
+++ src/Vargen.c
@@ -2,7 +2,7 @@
   This file is part of CDO. CDO is a collection of Operators to
   manipulate and analyse Climate model Data.
 
-  Copyright (C) 2003-2011 Uwe Schulzweida, Uwe.Schulzweida@zmaw.de
+  Copyright (C) 2003-2012 Uwe Schulzweida, Uwe.Schulzweida@zmaw.de
   See COPYING file for copying and redistribution conditions.
 
   This program is free software; you can redistribute it and/or modify
@@ -93,7 +93,7 @@ void *Vargen(void *argument)
   int operatorID;
   int streamID;
   int nrecs,nvars, ntimesteps, nlevels = 1;
-  int tsID, recID, varID, varID2, levelID;
+  int tsID, recID, varID, varID2 = -1, levelID;
   int gridsize, i;
   int vlistID;
   int gridID = -1, zaxisID, taxisID;
