bc76d5fad42ddc3d1a3b452dd4421713f2ebc921
Ralf Mueller <ralf.mueller@mpimet.mpg.de>
2012-08-30 12:31:05 +0000

    Add test for showunit

--- test/test_info.py
+++ test/test_info.py
@@ -12,11 +12,15 @@ class TestInfo(unittest.TestCase):
         self.assertEqual("File format: GRIB",info[0])
 
         sinfov = cdo.sinfov(input = "-stdatm,0,10,20,50,100,500,1000",options = "-f nc")
-        infov  = cdo.infov( input = "-stdatm,0,10,20,50,100,500,1000",options = "-f nc")
         self.assertEqual("P",sinfov[2].split(' ')[6])
         self.assertEqual("T",sinfov[3].split(' ')[6])
+
+        infov  = cdo.infov( input = "-stdatm,0,10,20,50,100,500,1000",options = "-f nc")
         self.assertEqual("1013.2", infov[1].split(' ')[-1])
         self.assertEqual("T", infov[-1].split(' ')[4])
+
+        units  = cdo.showunit( input = "-stdatm,0", options = "-f nc")
+        self.assertEqual(["hPa","K"],units[0].split(' '))
 
         levels = cdo.showlevel(input = "-stdatm,0")
         self.assertEqual([0,0],map(float,levels))
