79e35735b0555c93ed06dba061c2d9c8f6f38acd
Ralf Mueller <ralf.mueller@mpimet.mpg.de>
2012-08-30 10:36:39 +0000

    Add test for old style sinfov output

--- test/test_info.py
+++ test/test_info.py
@@ -12,6 +12,9 @@ class TestInfo(unittest.TestCase):
         self.assertEqual([0,0],map(float,levels))
         self.assertEqual("File format: GRIB",info[0])
 
+        sinfov = cdo.sinfov(input = "-stdatm,0,10,20,50,100,500,1000",options = "-f nc")
+        self.assertEqual("P",sinfov[2].split(' ')[6])
+        self.assertEqual("T",sinfov[3].split(' ')[6])
 
 if __name__ == '__main__':
     unittest.main()
