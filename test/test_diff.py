460be32d8b23e41ac62887691a87526dd1e791f6
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2012-09-10 13:03:35 +0000

    test_diff.py: changed test from operator random to const

--- test/test_diff.py
+++ test/test_diff.py
@@ -7,11 +7,11 @@ class TestDiff(unittest.TestCase):
 
     def test_diff(self):
         cdo = Cdo()
-        diffv = cdo.diffn(input = "-random,r1x1 -random,r1x1")
-        self.assertEqual(diffv[1].split(' ')[-1],"random")
-        self.assertEqual(diffv[1].split(' ')[-3],"0.53060")
-        diff  = cdo.diff(input = "-random,r1x1 -random,r1x1")
-        self.assertEqual(diff[1].split(' ')[-3],"0.53060")
+        diffv = cdo.diffn(input = "-const,4,r1x1 -const,2,r1x1")
+        self.assertEqual(diffv[1].split(' ')[-1],"const")
+        self.assertEqual(diffv[1].split(' ')[-3],"0.50000")
+        diff  = cdo.diff(input = "-const,4,r1x1 -const,2,r1x1")
+        self.assertEqual(diff[1].split(' ')[-3],"0.50000")
 
 
 if __name__ == '__main__':
