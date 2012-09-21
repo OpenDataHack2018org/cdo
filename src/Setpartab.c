84abeaa160b1df3e7966fe695518b0f6d98bf8c4
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2012-09-21 12:00:00 +0000

    setpartab: added units support

--- src/Setpartab.c
+++ src/Setpartab.c
@@ -29,10 +29,12 @@
 #  include <udunits2/udunits2.h>
 #endif
 
+#include <errno.h>
 #include <cdi.h>
 #include "cdo.h"
 #include "cdo_int.h"
 #include "pstream.h"
+#include "util.h"
 #include "namelist.h"
 
 
@@ -40,15 +42,29 @@
 ut_system *ut_read = NULL;
 
 static
-void *get_converter(char *src_unit_str, char *tgt_unit_str)
+void *get_converter(char *src_unit_str, char *tgt_unit_str, int *rstatus)
 {
   ut_unit *src_unit, *tgt_unit;
   cv_converter *ut_units_converter = NULL;
+  int status;
+
+  *rstatus = -1;
 
   if ( ut_read == NULL )
     {
+      errno = 0;
       ut_read = ut_read_xml(NULL);
-      if ( ut_get_status() != UT_SUCCESS )
+      status = ut_get_status();
+      if ( status == UT_PARSE )
+	{
+	  if ( cdoVerbose ) cdoWarning("Udunits: Couldn't parse unit database!");
+	}
+      if ( status == UT_OPEN_ENV || status == UT_OPEN_DEFAULT || status == UT_OS )
+	{
+	  if ( cdoVerbose ) cdoWarning("Udunits: %s", strerror(errno));
+	}
+      errno = 0;
+      if ( status != UT_SUCCESS )
 	{
 	  if ( cdoVerbose ) cdoWarning("Udunits: Error reading units system!");
 	  return NULL;
@@ -59,7 +75,7 @@ void *get_converter(char *src_unit_str, char *tgt_unit_str)
   src_unit = ut_parse(ut_read, src_unit_str, UT_ASCII);
   if ( ut_get_status() != UT_SUCCESS )
     {
-      if ( cdoVerbose ) cdoWarning("Udunits: Error parsing units: %s", src_unit_str);
+      if ( cdoVerbose ) cdoWarning("Udunits: Error parsing units: [%s]", src_unit_str);
       return NULL;
     }
 
@@ -67,28 +83,41 @@ void *get_converter(char *src_unit_str, char *tgt_unit_str)
   tgt_unit = ut_parse(ut_read, tgt_unit_str, UT_ASCII);
   if ( ut_get_status() != UT_SUCCESS )
     {
-      if ( cdoVerbose ) cdoWarning("Udunits: Error parsing units: %s", tgt_unit_str);
+      if ( cdoVerbose ) cdoWarning("Udunits: Error parsing units: [%s]", tgt_unit_str);
       return NULL;
     }
 
-  ut_units_converter = ut_get_converter(src_unit, tgt_unit);
-  if ( ut_get_status() != UT_SUCCESS )
+  status = ut_compare(src_unit, tgt_unit);
+  if ( status == 0 ) *rstatus = -2;
+
+  if ( *rstatus == -1 )
     {
-      if ( cdoVerbose ) cdoWarning("Udunits: Error getting converter from %s to %s", src_unit_str, tgt_unit_str);
-      return NULL;
+      status = ut_are_convertible(src_unit, tgt_unit);
+      if ( status == 0 ) *rstatus = -3;
+    }
+
+  if ( *rstatus == -1 )
+    {
+      ut_units_converter = ut_get_converter(src_unit, tgt_unit);
+      if ( ut_units_converter == NULL || ut_get_status() != UT_SUCCESS )
+	{
+	  if ( cdoVerbose ) cdoWarning("Udunits: Error getting converter from [%s] to [%s]", src_unit_str, tgt_unit_str);
+	}
+      else
+	*rstatus = 0;
     }
 
   ut_free(src_unit);
   if ( ut_get_status() != UT_SUCCESS )
     {
-      if ( cdoVerbose ) cdoWarning("Udunits: Error freeing units %s", src_unit_str);
+      if ( cdoVerbose ) cdoWarning("Udunits: Error freeing units [%s]", src_unit_str);
       return NULL;
     }
      
   ut_free(tgt_unit);
   if ( ut_get_status() != UT_SUCCESS )
     {
-      if ( cdoVerbose ) cdoWarning("Udunits: Error freeing units %s", tgt_unit_str);
+      if ( cdoVerbose ) cdoWarning("Udunits: Error freeing units [%s]", tgt_unit_str);
       return NULL;
     }
 
@@ -308,18 +337,37 @@ void *Setpartab(void *argument)
 
 			  if ( memcmp(units, units_old, len2) != 0 )
 			    {
-			      if ( cdoVerbose ) cdoPrint("%s - change units from [%s] to [%s]", name, units_old, units);
 			      if ( len1 > 0 && len2 > 0 )
 				{
+				  int status;
 				  vars[varID].changeunits = TRUE;
 				  strcpy(vars[varID].units_old, units_old);
 				  strcpy(vars[varID].units, units);
 #if defined (HAVE_LIBUDUNITS2)
-				  vars[varID].ut_converter = get_converter(units_old, units);
+				  vars[varID].ut_converter = get_converter(units_old, units, &status);
 				  if ( vars[varID].ut_converter == NULL )
 				    {
-				      cdoWarning("%s - change units from [%s] to [%s] failed!", name, units_old, units);
+				      if ( status == -2 )
+					{
+					  if ( cdoVerbose )
+					    cdoPrint("%s - not converted from [%s] to [%s], units are equal!", name, units_old, units);
+					}
+				      else if ( status == -3 )
+					{
+					  cdoWarning("%s - converting units from [%s] to [%s] failed, not convertible!", name, units_old, units);
+					}
+				      else
+					cdoWarning("%s - converting units from [%s] to [%s] failed!", name, units_old, units);
 				      vars[varID].changeunits = FALSE;
+				    }
+				  else
+				    {
+				      if ( cdoVerbose )
+					{
+					  char buf[64];
+					  cv_get_expression(vars[varID].ut_converter, buf, 64, name);
+					  cdoPrint("%s - convert units from [%s] to [%s] (expression: %s).", name, units_old, units, buf);
+					}
 				    }
 #else
 				  if ( lwarn_udunits )
@@ -427,7 +475,7 @@ void *Setpartab(void *argument)
 		}
 	      if ( nerr )
 		{
-		  cdoWarning("Udunits: Error with converter (from %s to %s), parameter: %s",
+		  cdoWarning("Udunits: Error converting units from [%s] to [%s], parameter: %s",
 			     vars[varID].units_old, vars[varID].units, vars[varID].name);
 		  vars[varID].changeunits = FALSE;
 		}
