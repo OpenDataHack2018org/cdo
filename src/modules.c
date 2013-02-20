8b92677faff7eae4f1af9b93920c41060c6ca40e
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2013-02-20 18:10:58 +0000

    modules.c cleanup

--- src/modules.c
+++ src/modules.c
@@ -320,8 +320,7 @@ void *Maggraph(void *argument);
 #define  GatherOperators        {"gather"}
 #define  GengridOperators       {"gengrid"}
 #define  GradsdesOperators      {"gradsdes1", "gradsdes2", "dumpmap"}
-#define  GridboxstatOperators   {"gridboxmin", "gridboxmax", "gridboxsum", "gridboxmean", "gridboxavg", \
-                                 "gridboxvar", "gridboxstd"}
+#define  GridboxstatOperators   {"gridboxmin", "gridboxmax", "gridboxsum", "gridboxmean", "gridboxavg", "gridboxvar", "gridboxstd"}
 #define  GridcellOperators      {"gridarea", "gridweights", "gridmask", "griddx", "griddy"}
 #define  HarmonicOperators      {"harmonic"}
 #define  HistogramOperators     {"histcount", "histsum", "histmean", "histfreq"}
