175474889c9085795bc0f480368d2e102f1f0914
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2013-09-09 11:56:13 +0000

    added test/test_Spectral.sh

--- test/data/gen_refdata.sh
+++ test/data/gen_refdata.sh
@@ -4,6 +4,8 @@ CDO=cdo
 #
 FORMAT="-f srv -b 32"
 #
+# Timstat
+#
 IFILE=EH5_AMIP_1_TSURF_6H_1991-1995.grb
 OFILE=ts_6h_5years
 $CDO $FORMAT remapnn,lon=55_lat=10 $IFILE $OFILE
@@ -22,9 +24,29 @@ for STAT in $STATS; do
   $CDO $FORMAT tim$STAT $IFILE tim${STAT}_ref
 done
 #
+# Vertint
+#
 IFILE=ECHAM5_T21L19monavg.grb
 OFILE=hl_l19.grb
 $CDO fldmean -sp2gp -selcode,129,130,152 $IFILE $OFILE
 IFILE=$OFILE
 OFILE=ml2pl_ref
 $CDO $FORMAT ml2pl,92500,85000,50000,20000 $IFILE $OFILE
+#
+# Spectral
+#
+IFILE=ECHAM5_T21L19monavg.grb
+OFILE=t21_geosp_tsurf.grb
+$CDO selname,geosp,tsurf $IFILE $OFILE
+#
+IFILE=$OFILE
+OFILE=gp2sp_ref
+$CDO gp2sp $IFILE $OFILE
+OFILE=gp2spl_ref
+$CDO gp2spl $IFILE $OFILE
+IFILE=gp2sp_ref
+OFILE=sp2gp_ref
+$CDO sp2gp $IFILE $OFILE
+IFILE=gp2spl_ref
+OFILE=sp2gpl_ref
+$CDO sp2gpl $IFILE $OFILE
