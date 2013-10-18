832462e7b394dba5c8c16acbd03fc063034991e0
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2013-10-18 13:25:42 +0000

    test/test_Gridarea.sh: changed to steradians

--- test/test_Gridarea.sh
+++ test/test_Gridarea.sh
@@ -5,17 +5,20 @@ CDODEBUG=0
 if [ "$CDODEBUG" == 0 ]; then CDO="$CDO -s"; fi
 CDOOUT=cout
 CDOERR=cerr
-REFVAL="  5.10064e+14"
+REFVAL="12.5663706"
 GRIDS="global_5 global_2 global_1 global_.5 n32 n80 n160"
 RSTAT=0;
+PLANET_RADIUS=1
+export PLANET_RADIUS
 #
 for GRID in $GRIDS; do
-  GLOBAREA=`$CDO output -fldsum -gridarea -random,$GRID`
+  GLOBAREA=`$CDO outputf,%10.7f,1 -fldsum -gridarea -random,$GRID`
 #  echo "$GRID: >$GLOBAREA< >$REFVAL<"
   if [ "$GLOBAREA" != "$REFVAL" ]; then RSTAT=`expr $RSTAT + 1`; fi
 done
 #
 rm -f $CDOOUT $CDOERR
+unset PLANET_RADIUS
 #
 if [ "$CDODEBUG" == 1 ]; then
   echo "rstat: $RSTAT"
