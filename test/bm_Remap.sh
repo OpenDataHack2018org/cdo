9fa8845ee49d9e118ddc8865a21ea9fdcfcc4982
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2014-01-20 20:40:12 +0000

    test/bm_Remap.sh: added cons

--- test/bm_Remap.sh
+++ test/bm_Remap.sh
@@ -2,7 +2,7 @@
 #
 CDO=src/cdo
 #
-RMODS="bil bic nn con"
+RMODS="bil bic nn con cons"
 #
 cdo -setrtomiss,0,10000 -topo topo05
 cdo -setrtomiss,0,10000 -remapbil,global_0.05 -topo topo005
@@ -22,19 +22,19 @@ done
 #
 # result on hama:
 # =================
-#         bil    bic    nn   con
-# reg2d   5.5    6.8     9   256
+#         bil    bic    nn   con   cons
+# reg2d   5.5    6.8     9   256    263
 # curv   43.8   45.0   982   300
 #
 # result on bailung:
 # =================
-#         bil    bic    nn   con
+#         bil    bic    nn   con   cons
 # reg2d   4.2    5.1     7   223
 # curv   38.8   39.0   842   223
 #
 # result on blizzard:
 # ==================
-#         bil    bic    nn   con
+#         bil    bic    nn   con   cons
 # reg2d    15     19    18   528
 # curv    101    104  1936   528
 #
