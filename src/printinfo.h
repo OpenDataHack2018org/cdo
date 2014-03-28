f3baf4d42b710b4ed022d9a8d686cae56336fe0c
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2014-03-28 12:04:47 +0000

    printinfo.h update

--- src/printinfo.h
+++ src/printinfo.h
@@ -269,13 +269,13 @@ void printGridInfo(int vlistID)
 	}
       else if ( gridtype == GRID_SPECTRAL )
 	{
-	  fprintf(stdout, "           : points=%d  nsp=%d  truncation=%d\n", gridsize, gridsize/2, trunc);
+	  fprintf(stdout, "          : points=%d  nsp=%d  truncation=%d\n", gridsize, gridsize/2, trunc);
 	  fprintf(stdout, "%*s", nbyte0, "");
 	  fprintf(stdout, "            complexPacking=%d\n", gridInqComplexPacking(gridID));
 	}
       else if ( gridtype == GRID_FOURIER )
 	{
-	  fprintf(stdout, "          : points=%d  nfc=%d  truncation=%d\n", gridsize, gridsize/2, trunc);
+	  fprintf(stdout, "         : points=%d  nfc=%d  truncation=%d\n", gridsize, gridsize/2, trunc);
 	}
       else if ( gridtype == GRID_GME )
 	{
