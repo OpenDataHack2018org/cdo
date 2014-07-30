8578534edf762e2a60570a9f292d5af52fb145ff
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2014-07-30 11:15:01 +0000

    added bucket_search.h

---
+++ test/grid_search/bucket_search.h
@@ -0,0 +1,38 @@
+/**
+ * @file bucket_search.h
+ *
+ * @copyright Copyright  (C)  2013 Moritz Hanke <hanke@dkrz.de>
+ *
+ * @version 1.0
+ * @author Moritz Hanke <hanke@dkrz.de>
+ */
+/*
+ * Keywords:
+ * Maintainer: Moritz Hanke <hanke@dkrz.de>
+ * URL: https://redmine.dkrz.de/doc/YAC/html/index.html
+ *
+ * This file is part of YAC.
+ *
+ * YAC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * YAC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with YAC.  If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
+
+#ifndef BUCKET_SEARCH_H
+#define BUCKET_SEARCH_H
+
+#include "grid_search.h"
+
+struct grid_search * bucket_search_new (struct grid * grid_data);
+
+#endif
+ 
