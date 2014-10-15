2b19d60f43112fc232ab992c00afa021e23ab984
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2014-10-15 13:52:31 +0000

    cdoAbort: close all open CDI streams

--- src/exception.c
+++ src/exception.c
@@ -30,7 +30,7 @@ void cdiError(int cdiErrno, const char *fmt, ...)
   if ( _ExitOnError ) exit(EXIT_FAILURE);
 }
 
-
+void pstreamCloseAll(void);
 void cdoAbort(const char *fmt, ...)
 {
   va_list args;
