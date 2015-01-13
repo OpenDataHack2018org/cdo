eb434dd414b60140f9b0add0c5568f0d644ab806
Yvonne Kuestermann <yvonne.kuestermann@mpimet.mpg.de>
2015-01-13 16:19:43 +0000

    refactor cdo.h: moved extern vars to util.h

--- src/util.h
+++ src/util.h
@@ -18,6 +18,68 @@
 #ifndef _UTIL_H
 #define _UTIL_H
 
+
+/* dummy use of unused parameters to silence compiler warnings */
+#define  UNUSED(x) (void)x
+
+#undef   TRUE
+#define  TRUE   1
+#undef   FALSE
+#define  FALSE  0
+
+#undef   MIN
+#define  MIN(a,b)  ((a) < (b) ? (a) : (b))
+#undef   MAX
+#define  MAX(a,b)  ((a) > (b) ? (a) : (b))
+
+#define  ADD_PLURAL(n)  ((n)>1 ? "s" : "")
+
+#define  UNCHANGED_RECORD  (processSelf() == 0 && cdoStreamName(0)->argv[0][0] != '-' && cdoRegulargrid == FALSE && cdoDefaultFileType == -1 && cdoDefaultDataType == -1 && cdoDefaultByteorder == -1 )
+
+
+extern int ompNumThreads;
+
+extern int stdin_is_tty;
+extern int stdout_is_tty;
+extern int stderr_is_tty;
+
+extern int cdoDefaultFileType;
+extern int cdoDefaultDataType;
+extern int cdoDefaultByteorder;
+extern int cdoDefaultTableID;
+extern int cdoDefaultInstID;
+
+extern int cdoLockIO;
+extern int cdoCheckDatarange;
+
+extern int cdoSilentMode;
+extern int cdoOverwriteMode;
+extern int cdoRegulargrid;
+extern int cdoBenchmark;
+extern int cdoTimer;
+extern int cdoVerbose;
+extern int cdoDebug;
+extern int cdoCompress;
+extern int cdoInteractive;
+extern int cdoParIO;
+
+extern int cdoCompType;
+extern int cdoCompLevel;
+
+extern int cdoChunkType;
+
+extern int cdoExpMode;
+
+extern int CDO_Color;
+extern int CDO_Use_FFTW;
+extern int cdoDiag;
+
+extern int cdoNumVarnames;
+extern char **cdoVarnames;
+
+int cdo_omp_get_thread_num(void);
+
+
 extern char CDO_Version[]; // refactored: moved here from pstream.c
 
 void strtolower(char *str);
