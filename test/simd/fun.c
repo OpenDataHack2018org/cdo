d2d76a2f44131f5be2ad96b49df24fb19aad799d
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2015-02-10 09:17:00 +0000

    simd cleanup

--- test/simd/fun.c
+++ test/simd/fun.c
@@ -1,5 +1,8 @@
-void fun1(int nelem, double *restrict array1, double *restrict array2)
+void fun1(int nelem, double *restrict array1, const double *restrict array2)
 {
+#if defined(_OPENMP)
+#pragma  omp simd aligned(array1:64) aligned(array2:64)
+#endif
   for ( int i = 0; i < nelem; ++i )
     array1[i] += array2[i];
 }
--- test/simd/memalign.c
+++ test/simd/memalign.c
@@ -1,9 +1,41 @@
-// icc -std=c99 -O3 -qopt-report=5 -march=native -openmp memalign.c fun.c
+// icc -std=c99 -O3 -march=native -qopt-report=5 -openmp memalign.c fun.c
+// gcc -std=c99 -O3 -march=native -ftree-vectorize -fdump-tree-vect-blocks -fopt-info-optimized -fopenmp memalign.c fun.c
 
+#ifndef _XOPEN_SOURCE
+#define _XOPEN_SOURCE 600
+#endif
+/*
+#ifndef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 200112L
+#endif
+*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 void fun1(int nelem, double *restrict array1, double *restrict array2);
+
+void print_opt(void)
+{
+  fprintf(stderr, "opt: ");
+#if defined(__AVX2__)
+  fprintf(stderr, " AVX2");
+#elif defined(__AVX__)
+  fprintf(stderr, " AVX");
+#elif defined(__SSE4_2__)
+  fprintf(stderr, " SSE4_2");
+#elif defined(__SSE4_1__)
+  fprintf(stderr, " SSE4_1");
+#elif defined(__SSE3__)
+  fprintf(stderr, " SSE3");
+#elif defined(__SSE2__)
+  fprintf(stderr, " SSE2");
+#endif 
+#if defined(_OPENMP)
+  fprintf(stderr, " OPENMP");  
+#endif
+  fprintf(stderr, "\n");
+}
 
 int get_alignment(double *ptr)
 {
@@ -26,12 +58,14 @@ int main(void)
   int nelem = 97;
   double *array1, *array2;
 
+  print_opt();
+
   malloc(nelem*sizeof(double));
   //array1 = (double *) malloc(nelem*sizeof(double));
-  posix_memalign(&array1, 32, nelem*sizeof(double));
+  posix_memalign((void **)&array1, 64, nelem*sizeof(double));
   malloc(nelem*sizeof(double));
   //array2 = (double *) malloc(nelem*sizeof(double));
-  posix_memalign(&array2, 32, nelem*sizeof(double));
+  posix_memalign((void **)&array2, 64, nelem*sizeof(double));
 
   printf("mem alignment: %d %d\n", get_alignment(array1), get_alignment(array2));
 
