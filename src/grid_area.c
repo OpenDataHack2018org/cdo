cfa5faf8552e3b7b03389a4ec873dac57fc96a6d
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2016-08-12 10:55:35 +0200

    Renamed gridXXXLCC to gridXXXParamLCC().

--- src/grid_area.c
+++ src/grid_area.c
@@ -297,7 +297,6 @@ int gridGenArea(int gridID, double* area)
   if ( gridtype != GRID_LONLAT      &&
        gridtype != GRID_GAUSSIAN    &&
        gridtype != GRID_LCC         &&
-       gridtype != GRID_LCC2        &&
        projtype != CDI_PROJ_RLL     &&
        projtype != CDI_PROJ_LAEA    &&
        projtype != CDI_PROJ_SINU    &&
