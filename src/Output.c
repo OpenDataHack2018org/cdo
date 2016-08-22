0040a38b2df52b89cd45bc3d04065c974a57c333
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2016-08-22 08:14:17 +0200

    Merge declaration and definition.

--- src/Output.c
+++ src/Output.c
@@ -36,37 +36,18 @@
 
 void *Output(void *argument)
 {
-  int OUTPUT, OUTPUTINT, OUTPUTSRV, OUTPUTEXT, OUTPUTF, OUTPUTTS, OUTPUTFLD, OUTPUTARR, OUTPUTXYZ, OUTPUTTAB;
-  int operatorID;
-  int i;
-  int indf;
-  int varID, recID;
-  int gridsize = 0;
-  int gridID, zaxisID, code, vdate, vtime;
-  int param;
-  int gridtype;
-  int ngrids;
+  int varID;
+  int gridID;
   int nrecs;
   int levelID;
-  int tsID, taxisID;
-  int streamID = 0;
-  int vlistID;
-  int nmiss, nout;
-  int nlon, nlat;
+  int nmiss;
   int nelem = 1;
   int len;
   int index;
-  int ndiffgrids;
-  int lhead = TRUE;
   const char *format = NULL;
   char paramstr[32];
   char vdatestr[32], vtimestr[32];
-  double level;
   double *grid_center_lon = NULL, *grid_center_lat = NULL;
-  double *array = NULL;
-  double xdate;
-  double missval;
-  double lon, lat;
   char name[CDI_MAX_NAME];
   int year, month, day;
   int *keys = NULL, nkeys = 0, k;
@@ -78,20 +59,20 @@ void *Output(void *argument)
 
   cdoInitialize(argument);
 
-  OUTPUT    = cdoOperatorAdd("output",    0, 0, NULL);
-  OUTPUTINT = cdoOperatorAdd("outputint", 0, 0, NULL);
-  OUTPUTSRV = cdoOperatorAdd("outputsrv", 0, 0, NULL);
-  OUTPUTEXT = cdoOperatorAdd("outputext", 0, 0, NULL);
-  OUTPUTF   = cdoOperatorAdd("outputf",   0, 0, NULL);
-  OUTPUTTS  = cdoOperatorAdd("outputts",  0, 0, NULL);
-  OUTPUTFLD = cdoOperatorAdd("outputfld", 0, 0, NULL);
-  OUTPUTARR = cdoOperatorAdd("outputarr", 0, 0, NULL);
-  OUTPUTXYZ = cdoOperatorAdd("outputxyz", 0, 0, NULL);
-  OUTPUTTAB = cdoOperatorAdd("outputtab", 0, 0, NULL);
+  int OUTPUT    = cdoOperatorAdd("output",    0, 0, NULL);
+  int OUTPUTINT = cdoOperatorAdd("outputint", 0, 0, NULL);
+  int OUTPUTSRV = cdoOperatorAdd("outputsrv", 0, 0, NULL);
+  int OUTPUTEXT = cdoOperatorAdd("outputext", 0, 0, NULL);
+  int OUTPUTF   = cdoOperatorAdd("outputf",   0, 0, NULL);
+  int OUTPUTTS  = cdoOperatorAdd("outputts",  0, 0, NULL);
+  int OUTPUTFLD = cdoOperatorAdd("outputfld", 0, 0, NULL);
+  int OUTPUTARR = cdoOperatorAdd("outputarr", 0, 0, NULL);
+  int OUTPUTXYZ = cdoOperatorAdd("outputxyz", 0, 0, NULL);
+  int OUTPUTTAB = cdoOperatorAdd("outputtab", 0, 0, NULL);
 
   UNUSED(OUTPUT);
 
-  operatorID = cdoOperatorID();
+  int operatorID = cdoOperatorID();
 
   if ( operatorID == OUTPUTF )
     {
@@ -104,19 +85,21 @@ void *Output(void *argument)
     }
   else if ( operatorID == OUTPUTTAB )
     {
+      bool lhead = true;
+
       operatorInputArg("keys to print");
  
       int npar = operatorArgc();
       char **parnames = operatorArgv();
 
       if ( cdoVerbose )
-	for ( i = 0; i < npar; i++ )
+	for ( int i = 0; i < npar; i++ )
 	  cdoPrint("key %d = %s", i+1, parnames[i]);
 
       keys = (int*) Malloc(npar*sizeof(int));
       nkeys = 0;
       nKeys = sizeof(Keynames)/sizeof(char *);
-      for ( i = 0; i < npar; i++ )
+      for ( int i = 0; i < npar; i++ )
 	{
 	  for ( k = 0; k < nKeys; ++k )
 	    {
@@ -125,7 +108,7 @@ void *Output(void *argument)
 	      if ( len < 3 ) len = 3;
 	      if ( strncmp(parnames[i], Keynames[k], len) == 0 )
 		{
-		  if ( k == knohead ) lhead = FALSE;
+		  if ( k == knohead ) lhead = false;
 		  else
 		    {
 		      keys[nkeys++] = k;
@@ -155,14 +138,14 @@ void *Output(void *argument)
 	}
     }
 
-  for ( indf = 0; indf < cdoStreamCnt(); indf++ )
+  for ( int indf = 0; indf < cdoStreamCnt(); indf++ )
     {
-      streamID = streamOpenRead(cdoStreamName(indf));
+      int streamID = streamOpenRead(cdoStreamName(indf));
 
-      vlistID = streamInqVlist(streamID);
+      int vlistID = streamInqVlist(streamID);
 
-      ngrids = vlistNgrids(vlistID);
-      ndiffgrids = 0;
+      int ngrids = vlistNgrids(vlistID);
+      int ndiffgrids = 0;
       for ( index = 1; index < ngrids; index++ )
 	if ( vlistGrid(vlistID, 0) != vlistGrid(vlistID, index) )
 	  ndiffgrids++;
@@ -170,10 +153,10 @@ void *Output(void *argument)
       if ( ndiffgrids > 0 ) cdoAbort("Too many different grids!");
 
       gridID   = vlistGrid(vlistID, 0);
-      gridsize = gridInqSize(gridID);
-      gridtype = gridInqType(gridID);
+      int gridsize = gridInqSize(gridID);
+      int gridtype = gridInqType(gridID);
 
-      array = (double*) Malloc(gridsize*sizeof(double));
+      double *array = (double*) Malloc(gridsize*sizeof(double));
 
       if ( operatorID == OUTPUTFLD || operatorID == OUTPUTXYZ || operatorID == OUTPUTTAB )
 	{
@@ -199,32 +182,32 @@ void *Output(void *argument)
 	  }
 	}
 
-      tsID = 0;
-      taxisID = vlistInqTaxis(vlistID);
+      int tsID = 0;
+      int taxisID = vlistInqTaxis(vlistID);
       while ( (nrecs = streamInqTimestep(streamID, tsID)) )
 	{
-	  vdate = taxisInqVdate(taxisID);
-	  vtime = taxisInqVtime(taxisID);
+	  int vdate = taxisInqVdate(taxisID);
+	  int vtime = taxisInqVtime(taxisID);
 	  date2str(vdate, vdatestr, sizeof(vdatestr));
 	  time2str(vtime, vtimestr, sizeof(vtimestr));
 
 	  cdiDecodeDate(vdate, &year, &month, &day);
 
-	  for ( recID = 0; recID < nrecs; recID++ )
+	  for ( int recID = 0; recID < nrecs; recID++ )
 	    {
 	      streamInqRecord(streamID, &varID, &levelID);
 
 	      vlistInqVarName(vlistID, varID, name);
-	      param    = vlistInqVarParam(vlistID, varID);
-	      code     = vlistInqVarCode(vlistID, varID);
-	      gridID   = vlistInqVarGrid(vlistID, varID);
-	      zaxisID  = vlistInqVarZaxis(vlistID, varID);
-	      missval  = vlistInqVarMissval(vlistID, varID);
-	      gridsize = gridInqSize(gridID);
-	      nlon     = gridInqXsize(gridID);
-	      nlat     = gridInqYsize(gridID);
-	      level    = zaxisInqLevel(zaxisID, levelID);
-	      
+	      int param    = vlistInqVarParam(vlistID, varID);
+	      int code     = vlistInqVarCode(vlistID, varID);
+	      int gridID   = vlistInqVarGrid(vlistID, varID);
+	      int zaxisID  = vlistInqVarZaxis(vlistID, varID);
+	      int gridsize = gridInqSize(gridID);
+	      int nlon     = gridInqXsize(gridID);
+	      int nlat     = gridInqYsize(gridID);
+	      double level    = zaxisInqLevel(zaxisID, levelID);
+              double missval  = vlistInqVarMissval(vlistID, varID);
+   
 	      cdiParamToString(param, paramstr, sizeof(paramstr));
 
 	      if ( nlon*nlat != gridsize ) { nlon = gridsize; nlat = 1; }
@@ -239,8 +222,8 @@ void *Output(void *argument)
 		
 	      if ( operatorID == OUTPUTINT )
 		{
-		  nout = 0;
-		  for ( i = 0; i < gridsize; i++ )
+		  int nout = 0;
+		  for ( int i = 0; i < gridsize; i++ )
 		    {
 		      if ( nout == 8 )
 			{
@@ -254,8 +237,8 @@ void *Output(void *argument)
 		}
 	      else if ( operatorID == OUTPUTF )
 		{
-		  nout = 0;
-		  for ( i = 0; i < gridsize; i++ )
+		  int nout = 0;
+		  for ( int i = 0; i < gridsize; i++ )
 		    {
 		      if ( nout == nelem )
 			{
@@ -283,28 +266,26 @@ void *Output(void *argument)
 		{
 		  int hour, minute, second;
 		  cdiDecodeTime(vtime, &hour, &minute, &second);
-		  xdate  = vdate - (vdate/100)*100 + (hour*3600 + minute*60 + second)/86400.;
-		  for ( i = 0; i < gridsize; i++ )
+		  double xdate = vdate - (vdate/100)*100 + (hour*3600 + minute*60 + second)/86400.;
+		  for ( int i = 0; i < gridsize; i++ )
 		    if ( !DBL_IS_EQUAL(array[i], missval) )
 		      fprintf(stdout, "%g\t%g\t%g\t%g\n", xdate, 
 			      grid_center_lat[i], grid_center_lon[i], array[i]);
 		}
 	      else if ( operatorID == OUTPUTTAB )
 		{
-		  int xind, yind;
-		  int l2d = FALSE;
-
+		  bool l2d = false;
 		  int xsize = gridInqXsize(gridID);
 		  // int ysize = gridInqYsize(gridID);
-		  if ( gridtype == GRID_CURVILINEAR ) l2d = TRUE;
+		  if ( gridtype == GRID_CURVILINEAR ) l2d = true;
 		      
-		  for ( i = 0; i < gridsize; i++ )
+		  for ( int i = 0; i < gridsize; i++ )
 		    {
-		      yind = i;
-		      xind = i;
+		      int yind = i;
+		      int xind = i;
 		      if ( l2d ) { yind /= xsize; xind -= yind*xsize; }
-		      lon = grid_center_lon[i];
-		      lat = grid_center_lat[i];
+		      double lon = grid_center_lon[i];
+		      double lat = grid_center_lat[i];
 
 		      for ( k = 0; k < nkeys; ++k )
 			{
@@ -337,25 +318,24 @@ void *Output(void *argument)
 		  if ( tsID == 0 && recID == 0 )
 		    {
 		      const char *fname = "frontplane.xyz";
-		      FILE *fp;
 		      double fmin = 0;
-		      double dx, x0, y0, z0, x, y, z;
-		      for ( i = 0; i < gridsize; i++ )
+		      double x, y, z;
+		      for ( int i = 0; i < gridsize; i++ )
 			if ( !DBL_IS_EQUAL(array[i], missval) )
 			  {
 			    if ( array[i] < fmin ) fmin = array[i];
 			    fprintf(stdout, "%g\t%g\t%g\t%g\n",
 				    grid_center_lon[i], grid_center_lat[i], array[i], array[i]);
 			  }
-		      fp = fopen(fname, "w");
+		      FILE *fp = fopen(fname, "w");
 		      if ( fp == NULL ) cdoAbort("Open failed on %s", fname);
 		      // first front plane
-		      dx = (grid_center_lon[1] - grid_center_lon[0]);
-		      x0 = grid_center_lon[0]-dx/2;
-		      y0 = grid_center_lat[0]-dx/2;
-		      z0 = fmin;
+		      double dx = (grid_center_lon[1] - grid_center_lon[0]);
+		      double x0 = grid_center_lon[0]-dx/2;
+		      double y0 = grid_center_lat[0]-dx/2;
+                      double z0 = fmin;
 		      fprintf(fp, ">\n");
-		      for ( i = 0; i < nlon; ++i )
+		      for ( int i = 0; i < nlon; ++i )
 			{
 			  x = x0;  y = y0; z = z0;
 			  fprintf(fp, "%g %g %g\n", x, y, z);
@@ -375,7 +355,7 @@ void *Output(void *argument)
 		      y0 = grid_center_lat[0]-dx/2;
 		      z0 = fmin;
 		      fprintf(fp, ">\n");
-		      for ( i = 0; i < nlat; ++i )
+		      for ( int i = 0; i < nlat; ++i )
 			{
 			  x = x0;  y = y0; z = z0;
 			  fprintf(fp, "%g %g %g\n", x, y, z);
@@ -395,10 +375,9 @@ void *Output(void *argument)
 		}
 	      else if ( operatorID == OUTPUTARR )
 		{
-		  for ( i = 0; i < gridsize; i++ )
+		  for ( int i = 0; i < gridsize; i++ )
 		    {
 		      fprintf(stdout, "  arr[%d] = %12.6g;\n", i, array[i]);
-		      nout++;
 		    }
 		}
 	      else
@@ -408,7 +387,7 @@ void *Output(void *argument)
 		  maxval = array[0];
 		  if ( gridInqType(gridID) == GRID_SPECTRAL && gridsize <= 156 )
 		    {
-		      for ( i = 1; i < gridsize; i++ )
+		      for ( int i = 1; i < gridsize; i++ )
 			{
 			  if ( array[i] < minval ) minval = array[i];
 			  if ( array[i] > maxval ) maxval = array[i];
@@ -418,9 +397,9 @@ void *Output(void *argument)
 		  if ( gridInqType(gridID) == GRID_SPECTRAL && gridsize <= 156 /* T11 */ &&
 		       minval >= -1 && maxval <= 12 )
 		    {
-		      long m, n, ntr;
+		      long m, n;
 		      double *spc = array;
-		      ntr = gridInqTrunc(gridID);
+		      long ntr = gridInqTrunc(gridID);
 		      for ( m = 0; m <= ntr; m++ )
 			{
 			  for ( n = m; n <= ntr; n++ )
@@ -433,8 +412,8 @@ void *Output(void *argument)
 		    }
 		  else
 		    {
-		      nout = 0;
-		      for ( i = 0; i < gridsize; i++ )
+		      int nout = 0;
+		      for ( int i = 0; i < gridsize; i++ )
 			{
 			  if ( nout == 6 )
 			    {
