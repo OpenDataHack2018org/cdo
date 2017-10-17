43787790d2da9c4fbf7c15f68b0c8d7e7ff7933c
Oliver Heidmann <oliver.heidmann@hotmail.de>
2017-10-17 16:59:43 +0200

    fixed macro error

--- src/cdoDebugOutput.cc
+++ src/cdoDebugOutput.cc
@@ -4,13 +4,13 @@
 
 namespace CdoDebug{
 
-#if defined(DEBUG-PSTREAM) || defined(DEBUG)
+#if defined(DEBUG_PSTREAM) || defined(DEBUG)
    int PSTREAM = 1;
 #else
    int PSTREAM = 0;
 #endif
 
-#if defined(DEBUG-PROCESS) || defined(DEBUG)
+#if defined(DEBUG_PROCESS) || defined(DEBUG)
    bool PROCESS = true;
 #else 
    bool PROCESS = false;
