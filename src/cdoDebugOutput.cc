217d4ee7f252bd11ba5c5757356787c642da48e2
Oliver Heidmann <oliver.heidmann@hotmail.de>
2017-10-17 16:34:03 +0200

    added options for debugging though defines

--- src/cdoDebugOutput.cc
+++ src/cdoDebugOutput.cc
@@ -4,12 +4,18 @@
 
 namespace CdoDebug{
 
-#ifdef DEBUG
+#if defined(DEBUG-PSTREAM) || defined(DEBUG)
    int PSTREAM = 1;
 #else
    int PSTREAM = 0;
 #endif
+
+#if defined(DEBUG-PROCESS) || defined(DEBUG)
+   bool PROCESS = true;
+#else 
    bool PROCESS = false;
+#endif
+
    std::string outfile = "";
    bool print_to_seperate_file = false;
 }
