385954eb3ba36e0d5e57ebd66b59560e45d0df4a
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2017-10-28 14:33:27 +0200

    nearpt3c.h: removed extern C.

--- src/nearpt3c.h
+++ src/nearpt3c.h
@@ -6,12 +6,6 @@ typedef float Coord_T;
 #define NPT3SFACT 32000
 #define NPT3SCALE(x) (0.5+(x+1)*NPT3SFACT)
 
-#if defined(__cplusplus)
-extern "C" {
-#endif
 void *nearpt3_preprocess(const int nfixpts, Coord_T **pts);
 int nearpt3_query(void *g, const Coord_T *q);
 void nearpt3_destroy(void *g);
-#if defined(__cplusplus)
-}
-#endif
