d8dc421bc4827df04c39ac3fcefe37c1687d451c
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2017-12-30 09:50:11 +0100

    Changed 2017 to 2018.

--- src/modules.h
+++ src/modules.h
@@ -1,7 +1,7 @@
 
   /***
 
-   Copyright (C) 2003-2017 Uwe Schulzweida, <uwe.schulzweida AT mpimet.mpg.de>
+   Copyright (C) 2003-2018 Uwe Schulzweida, <uwe.schulzweida AT mpimet.mpg.de>
    See COPYING file for copying and redistribution conditions.
 
    This program is free software; you can redistribute it and/or modify
