fd3419ecac7e9ec5bfc074743ee5f3c8541373f5
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2018-01-11 15:31:36 +0100

    Added Timpctl.test.in.

--- test/data/gen_refdata.sh
+++ test/data/gen_refdata.sh
@@ -5,6 +5,38 @@ CDO=cdo
 FORMAT="-f srv -b F32"
 ########################################################################
 #
+# Timpctl Yearpctl Monstat Daypctl
+#
+PCTLS="50"
+#
+IFILE=ts_mm_5years
+for PCTL in $PCTLS; do
+  $CDO $FORMAT seaspctl,${PCTL} $IFILE seasmin_ref seasmax_ref seaspctl${PCTL}_ref
+done
+#
+IFILE=ts_mm_5years
+for PCTL in $PCTLS; do
+  $CDO $FORMAT timpctl,$PCTL $IFILE timmin_ref timmax_ref timpctl${PCTL}_ref
+done
+#
+IFILE=ts_mm_5years
+for PCTL in $PCTLS; do
+  $CDO $FORMAT yearpctl,$PCTL $IFILE yearmin_ref yearmax_ref yearpctl${PCTL}_ref
+done
+#
+IFILE=ts_1d_1year
+for PCTL in $PCTLS; do
+  $CDO $FORMAT monpctl,$PCTL $IFILE monmin_ref monmax_ref monpctl${PCTL}_ref
+done
+#
+IFILE=ts_6h_1mon
+for PCTL in $PCTLS; do
+  $CDO $FORMAT daypctl,$PCTL $IFILE daymin_ref daymax_ref daypctl${PCTL}_ref
+done
+exit
+#
+########################################################################
+#
 # runpctl
 #
 IFILE=ts_mm_5years
