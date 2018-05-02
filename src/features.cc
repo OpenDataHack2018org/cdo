a36c710d4707d1711acddf09ea4cefb80640e7bc
Uwe Schulzweida <uwe.schulzweida@mpimet.mpg.de>
2018-05-02 10:23:30 +0200

    cdoConfig: added config parameter all-json.

--- src/features.cc
+++ src/features.cc
@@ -257,49 +257,74 @@ void cdoConfig(const char *option)
   has_openmp = YN[1];
 #endif
 
-  if      ( STR_IS_EQ("has-srv", option) ) fprintf(stdout, "%s\n", has_srv);
-  else if ( STR_IS_EQ("has-ext", option) ) fprintf(stdout, "%s\n", has_ext);
-  else if ( STR_IS_EQ("has-ieg", option) ) fprintf(stdout, "%s\n", has_ieg);
-  else if ( STR_IS_EQ("has-grb", option) ) fprintf(stdout, "%s\n", has_grb);
-  else if ( STR_IS_EQ("has-grb1", option) ) fprintf(stdout, "%s\n", has_grb);
-  else if ( STR_IS_EQ("has-grb2", option) ) fprintf(stdout, "%s\n", has_grb2);
-  else if ( STR_IS_EQ("has-nc", option) ) fprintf(stdout, "%s\n", has_nc);
-  else if ( STR_IS_EQ("has-nc2", option) ) fprintf(stdout, "%s\n", has_nc2);
-  else if ( STR_IS_EQ("has-nc4", option) ) fprintf(stdout, "%s\n", has_nc4);
-  else if ( STR_IS_EQ("has-nc4c", option) ) fprintf(stdout, "%s\n", has_nc4c);
-  else if ( STR_IS_EQ("has-nc5", option) ) fprintf(stdout, "%s\n", has_nc5);
-  else if ( STR_IS_EQ("has-hdf5", option) ) fprintf(stdout, "%s\n", has_hdf5);
-  else if ( STR_IS_EQ("has-cgribex", option) ) fprintf(stdout, "%s\n", has_cgribex);
-  else if ( STR_IS_EQ("has-cmor", option) ) fprintf(stdout, "%s\n", has_cmor);
-  else if ( STR_IS_EQ("has-proj", option) ) fprintf(stdout, "%s\n", has_proj);
-  else if ( STR_IS_EQ("has-threads", option) ) fprintf(stdout, "%s\n", has_threads);
-  else if ( STR_IS_EQ("has-openmp", option) ) fprintf(stdout, "%s\n", has_openmp);
+  if ( STR_IS_EQ("all-json", option) || STR_IS_EQ("all", option) )
+    {
+      fprintf(stdout, "{");
+      fprintf(stdout, "\"has-srv\":\"%s\"", has_srv);
+      fprintf(stdout, ",\"has-ext\":\"%s\"", has_ext);
+      fprintf(stdout, ",\"has-ieg\":\"%s\"", has_ieg);
+      fprintf(stdout, ",\"has-grb\":\"%s\"", has_grb);
+      fprintf(stdout, ",\"has-grb1\":\"%s\"", has_grb);
+      fprintf(stdout, ",\"has-grb2\":\"%s\"", has_grb2);
+      fprintf(stdout, ",\"has-nc\":\"%s\"", has_nc);
+      fprintf(stdout, ",\"has-nc2\":\"%s\"", has_nc2);
+      fprintf(stdout, ",\"has-nc4\":\"%s\"", has_nc4);
+      fprintf(stdout, ",\"has-nc4c\":\"%s\"", has_nc4c);
+      fprintf(stdout, ",\"has-nc5\":\"%s\"", has_nc5);
+      fprintf(stdout, ",\"has-hdf5\":\"%s\"", has_hdf5);
+      fprintf(stdout, ",\"has-cgribex\":\"%s\"", has_cgribex);
+      fprintf(stdout, ",\"has-cmor\":\"%s\"", has_cmor);
+      fprintf(stdout, ",\"has-proj\":\"%s\"", has_proj);
+      fprintf(stdout, ",\"has-threads\":\"%s\"", has_threads);
+      fprintf(stdout, ",\"has-openmp\":\"%s\"", has_openmp);
+      fprintf(stdout, "}\n");
+    }
   else
     {
-      fprintf(stdout, "unknown config option: %s\n", option);
-      fprintf(stdout, "\n");
-      fprintf(stdout, "Available config option:\n");
-      fprintf(stdout, "\n");
-      fprintf(stdout, "  has-srv     whether SERVICE is enabled\n");
-      fprintf(stdout, "  has-ext     whether EXTRA is enabled\n");
-      fprintf(stdout, "  has-ieg     whether IEG is enabled\n");
-      fprintf(stdout, "  has-grb     whether GRIB 1 is enabled\n");
-      fprintf(stdout, "  has-grb1    whether GRIB 1 is enabled\n");
-      fprintf(stdout, "  has-grb2    whether GRIB 2 is enabled\n");
-      fprintf(stdout, "  has-nc      whether NetCDF is enabled\n");
-      fprintf(stdout, "  has-nc2     whether NetCDF 2 is enabled\n");
-      fprintf(stdout, "  has-nc4     whether NetCDF 4 is enabled\n");
-      fprintf(stdout, "  has-nc4c    whether NetCDF 4 classic is enabled\n");
-      fprintf(stdout, "  has-nc5     whether NetCDF5 is enabled\n");
-      fprintf(stdout, "  has-hdf5    whether HDF5 is enabled\n");
-      fprintf(stdout, "  has-cgribex whether CGRIBEX is enabled\n");
-      fprintf(stdout, "  has-cmor    whether CMOR is enabled\n");
-      fprintf(stdout, "  has-proj    whether PROJ is enabled\n");
-      fprintf(stdout, "  has-threads whether PTHREADS is enabled\n");
-      fprintf(stdout, "  has-openmp  whether OPENMP is enabled\n");
-      
-      exit(EXIT_FAILURE);
+      if      ( STR_IS_EQ("has-srv", option) ) fprintf(stdout, "%s\n", has_srv);
+      else if ( STR_IS_EQ("has-ext", option) ) fprintf(stdout, "%s\n", has_ext);
+      else if ( STR_IS_EQ("has-ieg", option) ) fprintf(stdout, "%s\n", has_ieg);
+      else if ( STR_IS_EQ("has-grb", option) ) fprintf(stdout, "%s\n", has_grb);
+      else if ( STR_IS_EQ("has-grb1", option) ) fprintf(stdout, "%s\n", has_grb);
+      else if ( STR_IS_EQ("has-grb2", option) ) fprintf(stdout, "%s\n", has_grb2);
+      else if ( STR_IS_EQ("has-nc", option) ) fprintf(stdout, "%s\n", has_nc);
+      else if ( STR_IS_EQ("has-nc2", option) ) fprintf(stdout, "%s\n", has_nc2);
+      else if ( STR_IS_EQ("has-nc4", option) ) fprintf(stdout, "%s\n", has_nc4);
+      else if ( STR_IS_EQ("has-nc4c", option) ) fprintf(stdout, "%s\n", has_nc4c);
+      else if ( STR_IS_EQ("has-nc5", option) ) fprintf(stdout, "%s\n", has_nc5);
+      else if ( STR_IS_EQ("has-hdf5", option) ) fprintf(stdout, "%s\n", has_hdf5);
+      else if ( STR_IS_EQ("has-cgribex", option) ) fprintf(stdout, "%s\n", has_cgribex);
+      else if ( STR_IS_EQ("has-cmor", option) ) fprintf(stdout, "%s\n", has_cmor);
+      else if ( STR_IS_EQ("has-proj", option) ) fprintf(stdout, "%s\n", has_proj);
+      else if ( STR_IS_EQ("has-threads", option) ) fprintf(stdout, "%s\n", has_threads);
+      else if ( STR_IS_EQ("has-openmp", option) ) fprintf(stdout, "%s\n", has_openmp);
+      else
+        {
+          fprintf(stdout, "unknown config option: %s\n", option);
+          fprintf(stdout, "\n");
+          fprintf(stdout, "Available config option:\n");
+          fprintf(stdout, "\n");
+          fprintf(stdout, "  has-srv     whether SERVICE is enabled\n");
+          fprintf(stdout, "  has-ext     whether EXTRA is enabled\n");
+          fprintf(stdout, "  has-ieg     whether IEG is enabled\n");
+          fprintf(stdout, "  has-grb     whether GRIB 1 is enabled\n");
+          fprintf(stdout, "  has-grb1    whether GRIB 1 is enabled\n");
+          fprintf(stdout, "  has-grb2    whether GRIB 2 is enabled\n");
+          fprintf(stdout, "  has-nc      whether NetCDF is enabled\n");
+          fprintf(stdout, "  has-nc2     whether NetCDF 2 is enabled\n");
+          fprintf(stdout, "  has-nc4     whether NetCDF 4 is enabled\n");
+          fprintf(stdout, "  has-nc4c    whether NetCDF 4 classic is enabled\n");
+          fprintf(stdout, "  has-nc5     whether NetCDF5 is enabled\n");
+          fprintf(stdout, "  has-hdf5    whether HDF5 is enabled\n");
+          fprintf(stdout, "  has-cgribex whether CGRIBEX is enabled\n");
+          fprintf(stdout, "  has-cmor    whether CMOR is enabled\n");
+          fprintf(stdout, "  has-proj    whether PROJ is enabled\n");
+          fprintf(stdout, "  has-threads whether PTHREADS is enabled\n");
+          fprintf(stdout, "  has-openmp  whether OPENMP is enabled\n");
+          
+          exit(EXIT_FAILURE);
+        }
     }
-
+  
   exit(EXIT_SUCCESS);
 }
